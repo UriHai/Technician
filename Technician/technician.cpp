d46a0f8816ef408b9c13989d77981104ba798e93
uri <13288uri@gmail.com>
2025-07-01 17:38:50 +0300

    Add window and sleep for hour at startup

--- Technician/technician.cpp
+++ Technician/technician.cpp
@@ -3,6 +3,14 @@
 
 #define RUN_KEY_PATH ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run")
 #define KEY_NAME ("RemoteManagment")
+#define MESSAGE_BOX_TITLE ("Remote Managment")
+#define MESSAGE_BOX_TEXT ("Managment program is up")
+
+#define MILLISECONDS_IN_SECOND (100)
+#define SECONDS_IN_MINUTE (60)
+#define MINUTES_IN_HOUR (60)
+#define MILLISECONDS_IN_HOUR (MILLISECONDS_IN_SECOND * SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
+#define SLEEP_TIME (MILLISECONDS_IN_HOUR)
 
 using std::string;
 
@@ -18,8 +26,12 @@ int main(int nargs, char* args[]) {
 	status = RegQueryValueExA(keyHandle, KEY_NAME, 0, NULL, NULL, NULL);
 	if (status != ERROR_SUCCESS) {
 		status = RegSetValueExA(keyHandle, "RemoteManagment", 0, REG_SZ, data, path_size);
+		status = RegCloseKey(keyHandle);
+	} else {
+		status = RegCloseKey(keyHandle);
+		MessageBoxA(NULL, MESSAGE_BOX_TEXT, MESSAGE_BOX_TITLE, MB_OK | MB_ICONINFORMATION);
+		Sleep(SLEEP_TIME);
 	}
-	status = RegCloseKey(keyHandle);
 
 	return 0;
 }
